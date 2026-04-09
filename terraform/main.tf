b46f16971dcfb0d7d1982d8201d305ba4eb46a3b
Hossein Naserianrad <hnrad98@gmail.com>
2026-04-09 21:18:00 +0100

    provision gcs and bq in terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,5 +10,24 @@ terraform {
 
 provider "google" {
   project = var.project_id
-  region  = "us-central1"
+  region  = var.region
+}
+
+resource "google_storage_bucket" "data_lake" {
+  name          = "${var.gcs_bucket_name}-${var.project_id}"
+  location      = var.location
+  force_destroy = true
+
+  uniform_bucket_level_access = true
+}
+
+resource "google_bigquery_dataset" "stock_market" {
+  dataset_id                 = var.bq_dataset_name
+  location                   = var.location
+  delete_contents_on_destroy = true
+
+  labels = {
+    environment = "dev"
+    project     = "stock-pipeline"
+  }
 }
